e6b43a691e7c9cef06e50ea62983c779f9b1ff2f
Thisara Dasun <149833875+Thisara404@users.noreply.github.com>
2024-07-17 13:30:05 +0530

    add file

---
+++ thisra/java.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+
+public class{
+    public static void main (String[] args){
+
+Public Class{
+    
+
+        System.out.println("Hello World");
+
+    }
+}
---
+++ thisra/js/app.js
@@ -0,0 +1,4 @@
+const int [] array ={1,2,3,4,5};
+dgbhasgdbhjkasbdajkshdbsawqjk{
+    
+}
